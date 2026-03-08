ebc97aa95120f52d3c4d76921c2604ecdef5a7b8
vector-dev <vector@local.dev>
2026-03-09 06:49:28 +0700

    auto update

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d6684b2a1fa0f7e1"; }
+/******/ 		__webpack_require__.h = function() { return "0003d3f1e08e8eaf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
