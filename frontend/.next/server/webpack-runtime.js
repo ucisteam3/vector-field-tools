787e3fc5b7c86e00fddc298f4cc1a50eaefd51ed
vector-dev <vector@local.dev>
2026-03-09 12:04:05 +0700

    auto update

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1c8935a0fbb23327")
+/******/ 		__webpack_require__.h = () => ("c787ea377b5acf8f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
